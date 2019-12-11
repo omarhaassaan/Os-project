c39f194aa330a0b62168e67d72a6fc013aead3b0
omarhaassaan <omarhassan321@hotmail.com>
2019-12-11 16:34:15 +0200

    DeadLock Detection

--- 3/DeadLock/src/deadlock/DeadLock.java
+++ 3/DeadLock/src/deadlock/DeadLock.java
@@ -6,6 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 class Resource extends ReentrantLock {
 //represent the Thread ID Occupy particular resource 
     // Ex. : Thread1 => thread number 1
+    // thread number = -1 when Resource is free
+
     private int threadNumber = -1;
 
     public void lock(int threadNumber) {
@@ -29,7 +31,7 @@ class Thread1 extends Thread {
 
     Resource resources[] = new Resource[2];
 
-    Thread1(Resource [] resources) {
+    Thread1(Resource[] resources) {
         this.resources = resources;
     }
 
@@ -56,7 +58,7 @@ class Thread2 extends Thread {
 
     Resource resources[] = new Resource[2];
 
-    Thread2(Resource []r) {
+    Thread2(Resource[] r) {
         this.resources = r;
     }
 
@@ -65,14 +67,14 @@ class Thread2 extends Thread {
         resources[0].lock(2);
         try {
             Thread.sleep(10);
-            System.out.println("T1 " + resources[0]);
+            System.out.println("T2 " + resources[0]);
         } catch (Exception e) {
             System.out.println(e);
         }
         System.out.println(resources[1].isLocked());
         resources[1].lock(2);
         resources[0].unlock();
-        System.out.println("T1 " + resources[1]);
+        System.out.println("T2 " + resources[1]);
         resources[1].unlock();
     }
 }
@@ -80,9 +82,84 @@ class Thread2 extends Thread {
 //main program
 public class DeadLock {
 
-    public static void main(String[] args) {
+    //Test Function 
+    /*public static void prnt2d(int arr[][]) {
+        System.out.println("Need: ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(arr[i][j] + "|");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void prnt1d(int arr[]) {
+        System.out.println("Available: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "|");
+        }
+    }
+     */
+ /*
+    Compare 2 arrays
+    -1 => arr1 < arr2
+    0 => arr1 = arr2
+    1 => arr1 > arr2
+     */
+    public static int compare(int arr1[], int arr2[]) {
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] < arr2[i]) {
+                return -1;
+            } else if (arr1[i] > arr2[i]) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static boolean checkDeadLock(Resource[] resources, Thread t1, Thread t2) {
+        int[] available = new int[2];
+        int[][] need = new int[2][2];
+        //Check available resources and put it in an array
+        for (int i = 0; i < available.length; i++) {
+            if (resources[i].getThreadNumber() == -1) {
+                available[i] = 1;
+            } else {
+                available[i] = 0;
+            }
+        }
+        // initialize the need 2d array
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                need[i][j] = 1;
+            }
+        }
+        /* Check what each thread hold and put it with 0 
+        Example:
+           | R1 | R2
+        T1 | 0  | 1
+        T2 | 1  | 0
+        T1 needs R2, T2 needs R1
+         */
+        for (int i = 0; i < 2; i++) {
+            if (resources[i].getThreadNumber() != -1) {
+                need[i][resources[i].getThreadNumber() - 1] = 0;
+            }
+        }
+        //  prnt1d(available);
+        //  prnt2d(need);
+        //Check each thread needs to the available, if all need < available => no deadlock detected 
+        for (int i = 0; i < 2; i++) {
+            if (compare(need[i], available) == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         int NumberOfResources = 2;
-        Resource resources [] = new Resource[NumberOfResources];
+        Resource resources[] = new Resource[NumberOfResources];
         for (int i = 0; i < NumberOfResources; i++) {
             resources[i] = new Resource();
         }
@@ -90,5 +167,15 @@ public class DeadLock {
         Thread t2 = new Thread2(resources);
         t1.start();
         t2.start();
+        //Assume each resource can execute only one thread
+        //DeadLock detection
+        while (t1.isAlive() || t2.isAlive()) {
+            if (checkDeadLock(resources, t1, t2)) {
+                System.out.println("DeadLock Detected !!!!");
+                break;
+            }
+            Thread.sleep(20);
+        }
+
     }
 }
